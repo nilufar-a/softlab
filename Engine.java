3df26ccb2d58e41d3c95b4da7d62ad80c40c41de
Nilufer Aleskerli <42249942+nilufer454@users.noreply.github.com>
2019-03-24 01:23:32 +0100

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -1,24 +1,24 @@
 package lab;
 
 public class Engine extends Vehicle {
+	
 boolean isVisible; 
 
-
-public void move(Direction d) {
-	
-}
 public void pullCar(Direction d) {
-	
+	 System.out.print("engine pulls cars");
 }
+	
 public void crashWith (Vehicle v) {
-	
+	 System.out.print("engine crash with vehicle");
 }
+	
 public void setVisible() {
 	
 	isVisible=true;
+	 System.out.print("set visible");
 }
+	
 public void setInvisible() {
 	isVisible=false;
-}
-
-};
+ System.out.print("set invisible");
+}};
