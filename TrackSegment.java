d03a46c7b43550a470f822889ee226428bb1c8a2
sardarov <44386051+sardarov@users.noreply.github.com>
2019-03-25 01:35:44 +0100

    Update TrackSegment.java

--- TrackSegment.java
+++ TrackSegment.java
@@ -1,19 +1,37 @@
 package softlab;
 
+import java.util.ArrayList;
+
 public class TrackSegment {
-	private Car[] cars;
+	private ArrayList<Vehicle> vehicles;
 	private TrackSegment neighboorField1;
 	private TrackSegment neighboorField2;
 	
-	public void getNextSegment(Direction dir) {
-		
+	public TrackSegment() {
+		vehicles=new ArrayList<>();
 	}
-	
+	public TrackSegment getNextSegment(Direction dir) {
+		System.out.println("The next segment was returned");
+		return neighboorField1;
+	}
+	public boolean checkCollide() {
+		if (vehicles.size()>1) {
+			System.out.println("Collision was detected");
+			return true;
+		}
+		else
+		{
+			System.out.println("Collision wasn't detected");
+			return false;
+		}
+	}
 	public void accept(Vehicle v) {
-		
+		vehicles.add(v);
+		System.out.println(v.getClass().getSimpleName()+" was added to the trackSegment");
 	}
 	
-	public void remove(Vehicle c) {
-		
+	public void remove(Vehicle v) {
+		vehicles.remove(v);
+		System.out.println(v.getClass().getSimpleName()+" was deleted from the trackSegment");
 	}
 }
