b7e29b68f80bb6749ef249f4193c1c495fcf0028
sardarov <44386051+sardarov@users.noreply.github.com>
2019-03-25 01:34:01 +0100

    Update Map.java

--- Map.java
+++ Map.java
@@ -7,12 +7,14 @@ public class Map {
 	private int totalPassangers;
 	private ArrayList<TrackSegment> trackSegments;
 	
+	
 	public Map() {
 		trackSegments=new ArrayList<TrackSegment>();
 	}
 	
 	public void removePassengers(int count) {
 		totalPassangers-=count;
+		System.out.println("The number of Passanger was decreased by "+count);
 	}
 	public void addTrackSegment(TrackSegment ts) {
 		trackSegments.add(ts);
